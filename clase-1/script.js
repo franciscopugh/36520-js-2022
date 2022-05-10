6dd91f7dc44d0c1d4a55a8a08a082d1d11339194
Francisco Pugh <franciscopugh01@gmail.com>
2022-05-10 12:23:33 -0300

    Clase 1

---
+++ clase-1/script.js
@@ -0,0 +1,44 @@
+/*var nombre = "Francisco"
+
+console.log(nombre)
+
+var nombre = 5
+
+console.log(nombre)
+
+let sueldo = 100
+
+console.log(sueldo)
+
+sueldo = "Bastante"
+
+console.log(sueldo)
+
+const IVA = 1.21
+
+//IVA = 1.30
+
+console.log(100 * IVA)
+
+//string number boolean null undefined object
+Date()
+let array = [1,2,3]
+
+console.log(array[5])
+
+console.log( -5 / 0)*/
+
+let numero1 = parseInt(prompt("Por favor ingrese un numero"))
+let numero2 = parseFloat(prompt("Ingrese otro numero").replace(',','.'))
+let numero3 = Number(prompt("Ingrese otrooo numero"))
+
+/*if (isNaN(numero1)) {
+    console.log("V")
+} else {
+    console.log("F")
+}*/
+
+
+alert(numero1)
+alert(numero2)
+alert(numero3)
